41e843a99a1d0d8939edaadae6342ec56fa0f145
li0228 <2482519180@qq.com>
2020-05-30 19:28:25 +0800

    创建

---
+++ src/main/java/com/man/yin/myin/MyinApplication.java
@@ -0,0 +1,13 @@
+package com.man.yin.myin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/man/yin/myin/MyinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.man.yin.myin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
